8d76b5a329f69caa374ee2e9cf641594c338fe2c
Piotr <piotr.ostrowski@aol.com>
2021-09-15 13:15:52 +0200

    make all the requests execute at once

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
-import aiohttp
 import asyncio
-import pandas
+import pandas as pd
 
+from aiohttp import ClientSession
 
 # another thing that could be cool is algo buying fat orderbook breaks
 
-async def make_request(url: str):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            return await response.json()
+async def make_request(session: ClientSession, url: str):
+    async with session.get(url) as response:
+        return await response.json()
 
 
-async def get_candles(symbol: str, interval: str):
+async def get_candles(session: ClientSession, symbol: str, interval: str):
     base = 'https://api1.binance.com'
     slug = f'/api/v3/klines'
     params = f'?symbol={symbol}&interval={interval}'
-    return await make_request(base + slug + params)
+    return await make_request(session, base + slug + params)
 
 
-async def get_symbols():
+async def get_symbols(session: ClientSession):
     base = 'https://api1.binance.com'
     slug = f'/api/v1/exchangeInfo'
-    res = await make_request(base + slug)
+    res = await make_request(session, base + slug)
     return res['symbols']
 
 
 async def main():
-    symbols = [i['symbol'] for i in await get_symbols())]
-    candles = {}
-    for symbol in symbols:
-        candles[symbol] = await get_candles(symbol)
+    async with ClientSession() as session:
+        symbols = [i['symbol'] for i in await get_symbols(session)]
+        symbols = [symbol for symbol in symbols if 'USDT' in symbol]
+        symbols = symbols[:2]
+        tasks = []
+        for symbol in symbols:
+            task = asyncio.ensure_future(get_candles(session, symbol, '1h'))
+            tasks.append(task)
+        return (symbols, await asyncio.gather(*tasks))
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
-    
+    symbols, candles = asyncio.run(main())
+    res = dict(list(zip(symbols, candles)))
+
