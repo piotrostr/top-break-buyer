485afbae4d4d542cc819368644805888f319a61e
Piotr <piotr.ostrowski@aol.com>
2021-09-15 16:02:07 +0200

    add algo

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import asyncio
 import pandas as pd
+import numpy as np
+import talib
 
 from aiohttp import ClientSession
 
@@ -10,11 +12,17 @@ async def make_request(session: ClientSession, url: str):
         return await response.json()
 
 
-async def get_candles(session: ClientSession, symbol: str, interval: str):
+async def get_candles(session: ClientSession, 
+                      symbol: str, 
+                      interval: str) -> pd.DataFrame:
     base = 'https://api1.binance.com'
     slug = f'/api/v3/klines'
     params = f'?symbol={symbol}&interval={interval}'
-    return await make_request(session, base + slug + params)
+    res = await make_request(session, base + slug + params)
+    df = pd.DataFrame(res)
+    df = df[[0, 1, 2, 3, 4]]
+    df.columns = ['t', 'o', 'h', 'l', 'c']
+    return df
 
 
 async def get_symbols(session: ClientSession):
@@ -24,19 +32,33 @@ async def get_symbols(session: ClientSession):
     return res['symbols']
 
 
-async def main():
+async def get_data():
     async with ClientSession() as session:
         symbols = [i['symbol'] for i in await get_symbols(session)]
         symbols = [symbol for symbol in symbols if 'USDT' in symbol]
-        symbols = symbols[:2]
         tasks = []
         for symbol in symbols:
-            task = asyncio.ensure_future(get_candles(session, symbol, '1h'))
+            task = asyncio.ensure_future(get_candles(session, symbol, '15m'))
             tasks.append(task)
         return (symbols, await asyncio.gather(*tasks))
 
 
 if __name__ == '__main__':
-    symbols, candles = asyncio.run(main())
-    res = dict(list(zip(symbols, candles)))
+    out = []
+    symbols, _candles = asyncio.run(get_data())
+    for symbol, candles in zip(symbols, _candles):
+        candles = candles.astype(np.float64)
+        current_price = candles['c'].iloc[-1] 
+        high = max(candles['h'])
+        # and also add condition that the volume is increasing
+        atr_df = talib.ATR(candles['h'], candles['l'], candles['c'], 14)
+        atr = atr_df.iloc[-1]
+        if current_price > high:
+            if high + atr > current_price:
+                print(symbol)
+                out.append(candles)
+        else:
+            if current_price + atr > high:
+                print(symbol)
+                out.append(candles)
 
