28d7964d86fe6893d58ffc420ddb67f57dd19790
Piotr <piotr.ostrowski@aol.com>
2021-10-20 18:35:37 +0200

    add func for getting the trades, use more candles columns, change naming

--- main.py
+++ main.py
@@ -15,13 +15,24 @@ async def get_candles(session: ClientSession,
                       symbol: str, 
                       interval: str) -> pd.DataFrame:
     base = 'https://api1.binance.com'
-    slug = f'/api/v3/klines'
+    slug = '/api/v3/klines'
     params = f'?symbol={symbol}&interval={interval}'
     res = await make_request(session, base + slug + params)
     df = pd.DataFrame(res)
-    df = df[[0, 1, 2, 3, 4]]
-    df.columns = ['t', 'o', 'h', 'l', 'c']
+    df.columns = [
+        'open_time', 'open', 'high', 'low', 'close', 'volume',
+        'close_time', 'quote_volume', 'number_of_trades', 
+        'taker_buy_volume', 'taker_buy_quote_volume', '_'
+    ]
     return df
+
+
+async def get_recent_trades(symbol: str):
+    base = 'https://api1.binance.com'
+    slug = '/api/v3/aggTrades'
+    params = f'?symbol={symbol}'
+    res = await make_request(session, base + slug + params)
+    return res
 
 
 async def get_symbols(session: ClientSession):
@@ -42,19 +53,20 @@ async def get_data():
         return (symbols, await asyncio.gather(*tasks))
 
 
-if __name__ == '__main__':
-    # and also add condition that the volume is increasing
-    # say if this 100 candles > previous 100 candles volume profile or sth
-    # another thing that could be cool is algo buying fat orderbook breaks
-    # and the top has to be in a given distance from the current rally
-    # backtest 🥺
+async def main():
+    # this is pretty stupid, as 'close to top' assets might not be trending
     out = []
-    symbols, _candles = asyncio.run(get_data())
+    symbols, _candles = await get_data()
     for symbol, candles in zip(symbols, _candles):
         candles = candles.astype(np.float64)
-        current_price = candles['c'].iloc[-1] 
-        high = max(candles['h'])
-        atr_df = talib.ATR(candles['h'], candles['l'], candles['c'], 14)
+        current_price = candles['close'].iloc[-1] 
+        high = max(candles['high'])
+        atr_df = talib.ATR(
+            candles['high'], 
+            candles['low'], 
+            candles['close'], 
+            14
+        )
         atr = atr_df.iloc[-1]
         if current_price > high:
             if high + atr > current_price:
@@ -64,4 +76,20 @@ if __name__ == '__main__':
             if current_price + atr > high:
                 print(symbol)
                 out.append(candles)
+
+
+if __name__ == '__main__':
+    # TODO
+    # and also add condition that the volume is increasing
+    # say if this 100 candles > previous 100 candles volume profile or sth
+    # another thing that could be cool is algo buying fat orderbook breaks
+    # and the top has to be in a given distance from the current rally
+    # backtest 🥺
+
+    async def func(symbol: str, interval: str):
+        async with ClientSession() as session:
+            candles = await get_candles(session, symbol, interval)
+        return candles
+
+    df = asyncio.run(func('BTCUSDT', '1m'))
 
